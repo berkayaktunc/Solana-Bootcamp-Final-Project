722bae5bacc0032f461864c94befd8d716b4cd27
Berkay Aktunç <berkay.aktunc@gmail.com>
2024-01-27 15:29:08 +0300

    Wallet connection error fixed

--- final-project/src/components/AppBar.tsx
+++ final-project/src/components/AppBar.tsx
@@ -1,7 +1,16 @@
 import { FC } from "react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import dynamic from "next/dynamic";
+
 
 export const AppBar: FC = () => {
+    
+    const WalletMultiButton = dynamic(
+        async () =>
+          (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+        { ssr: false }
+      );
+
     return (
         <div className="flex w-full justify-end">
             <WalletMultiButton />
